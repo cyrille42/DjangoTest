ce7d7ef213c26c1e0cacb2f8f863a2472e12438c
cyrillegay1@gmail.com <cyrillegay1@gmail.com>
2022-03-17 14:05:47 +0100

    Chore: renaming variable, refactoring code

--- shop/stock/views.py
+++ shop/stock/views.py
@@ -40,11 +40,11 @@ class ProductList(generics.ListCreateAPIView):
         return product_list
 
 
-def check_number_of_product_left(cart_list, cart_serializer):
+def check_number_of_product_left(unique_product_list, product_list):
     error = ""
-    for product_id in cart_list:
+    for product_id in unique_product_list:
         product = Product.objects.get(pk=product_id)
-        if product.product_number < cart_serializer.validated_data.get('product').count(product_id):
+        if product.product_number < product_list.count(product_id):
             error += "The product " + product.product_name + " have only " + str(product.product_number) + " left\n"
     return error
 
@@ -60,9 +60,8 @@ class CartCreate(generics.ListCreateAPIView):
     def post(self, request):
         cart_serializer = CartSerializer(data=request.data, context={'request': request})
         if cart_serializer.is_valid():
-            cart_list = cart_serializer.validated_data.get('product').copy()
-            cart_list = set(cart_list)
-            error = check_number_of_product_left(cart_list, cart_serializer)
+            product_list = set(cart_serializer.validated_data.get('product').copy())
+            error = check_number_of_product_left(product_list, cart_serializer.validated_data.get('product'))
             if error:
                 return Response(error, status=status.HTTP_400_BAD_REQUEST)
             cart_serializer.save()
@@ -77,12 +76,35 @@ def nb_free_item(special_discount, special_discount_gift, nb_total):
     return max(0, r - special_discount) + (n * special_discount_gift)
 
 
+# Editing stock left from paid product
 def changing_paid_stock(product_id_list, total_product):
     for product_id in product_id_list:
         product = Product.objects.get(pk=product_id)
         product_paid = total_product.count(product_id)
         product.product_number = product.product_number - product_paid
         product.save()
+
+
+# Create data for ticket serilizer
+def get_ticket_data_from_cart(cart_serializer, product_id_list):
+    total_price = 0
+    product_paid = []
+    for product_id in product_id_list:
+        product = Product.objects.get(pk=product_id)
+        product_count = cart_serializer.validated_data.get('product').count(product_id)
+        discount_price = round(product.price - product.price * product.discount / 100, 2)
+        if product.special_discount > 0:
+            discounted_product = nb_free_item(product.special_discount, product.special_discount_gift, product_count)
+            product_count -= discounted_product
+            if discounted_product != 0:
+                product_paid.append([product_id, 0, 100, discounted_product])
+        product_paid.append([product_id, discount_price, product.discount, product_count])
+        total_price += discount_price * product_count
+        ticket_data = {
+                    'product_paid': product_paid,
+                    'total_amount': total_price
+                }
+        return ticket_data
 
 
 class CartDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -97,32 +119,17 @@ class CartDetail(generics.RetrieveUpdateDestroyAPIView):
         cart_serializer = CartDetailSerializer(data=request.data)
         if cart_serializer.is_valid():
             if cart_serializer.validated_data.get('validation') == True:
-                total_price = 0
-                product_id_list = list(set(cart_serializer.validated_data.get('product')))
-                product_paid = []
-                for product_id in product_id_list:
-                    product = Product.objects.get(pk=product_id)
-                    product_count = cart_serializer.validated_data.get('product').count(product_id)
-                    discount_price = round(product.price - product.price * product.discount / 100, 2)
-                    if product.special_discount > 0:
-                        discounted_product = nb_free_item(product.special_discount, product.special_discount_gift, product_count)
-                        product_count -= discounted_product
-                        if discounted_product != 0:
-                            product_paid.append([product_id, 0, 100, discounted_product])
-                    product_paid.append([product_id, discount_price, product.discount, product_count])
-                    total_price += discount_price * product_count
-                ticket_data = {
-                    'product_paid': product_paid,
-                    'total_amount': total_price
-                }
+                product_list = cart_serializer.validated_data.get('product')
+                unique_product_list = list(set(product_list))
+                ticket_data = get_ticket_data_from_cart(cart_serializer, product_list)
                 ticket_serializer = TicketSerializer(data=ticket_data)
                 if ticket_serializer.is_valid():
-                    error = check_number_of_product_left(product_id_list, cart_serializer)
+                    error = check_number_of_product_left(unique_product_list, product_list)
                     if error:
                         return Response(error, status=status.HTTP_400_BAD_REQUEST)
                     Cart.objects.filter(id=pk).delete()
                     ticket = ticket_serializer.save()
-                    changing_paid_stock(product_id_list, cart_serializer.validated_data.get('product'))
+                    changing_paid_stock(unique_product_list, product_list)
                     return Response("Cart deleted and ticket " + str(ticket.id) + " created", status=status.HTTP_201_CREATED)
                 return Response(ticket_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             return Response(cart_serializer.data, status=status.HTTP_201_CREATED)
